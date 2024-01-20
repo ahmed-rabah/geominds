a2b2099bf251f74f196d0b03678510b7d50f27e7
ahmed <ahmedrabah1937@gmail.com>
2024-01-20 04:35:54 +0100

    quiz game UI(no UX)

--- quiz.js
+++ quiz.js
@@ -53,14 +53,14 @@ function random(min =0, max) {
 //     let end  = duration ; 
 //     circle.style.strokeDashoffset  = dashArray - (dashArray * time) / end ; 
 //     let second = Math.ceil((time/1000)) ; 
-//     timer.lastElementChild.innerHTML = second+" seconds" ; 
+//     // document.getElementById('timer').lastElementChild.innerHTML = second+" seconds" ; 
 //     time  = time - 10 ; 
 //     if(time < 0){
 //         clearInterval(x)
 //     }
-//     if(time < ( end / 5 )){
-//         document.querySelector('svg circle:last-child').style.stroke = 'red' ; 
-//     }
+//     // if(time < ( end / 5 )){
+//     //     document.querySelector('svg circle:last-child').style.stroke = 'red' ; 
+//     // }
 //     // if(time < (end / 3)){
 //     //     document.querySelector('svg circle:first-child').style.stroke = 'red' ; 
 //     //     // document.querySelector('svg circle:last-child').style.stroke = 'red' ; 
