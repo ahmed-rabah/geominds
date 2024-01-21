048ec25835c1bcdf1f2dfcc4be73d136ae8ffcae
ahmed <ahmedrabah1937@gmail.com>
2024-01-21 05:41:06 +0100

    quiz game logic *1st

--- quiz.js
+++ quiz.js
@@ -4,6 +4,12 @@ import { getCountriesLevels ,  getCountries } from './components/utilities.js';
 let AllCountries =  getCountries() ;
 let levels = getCountriesLevels() ; 
 let startQuizBtn = document.querySelector('.quiz-start-btn') ; 
+let lifes = document.querySelector('.lifes') ; 
+let lifesElements = document.querySelectorAll('.lifes .life') ; 
+let textQuestion = document.querySelector('.question h3')
+let imageQuestion = document.querySelector('.question img')
+let optionsContainer = document.querySelector('.options')
+let optionsParag = document.querySelectorAll('.option') ; 
 
 
 // listeners
@@ -15,13 +21,88 @@ let hearts  = 3 ;
 let score  = 0 ; 
 let questionNbr = 0 ; 
 function startQuiz() {
-    
-    generateQuestion() ; 
+    let question  = GenerateQuestion() ; 
+    displayQuestion(question) ; 
 }
 
-function  generateQuestion(){
+
+function displayQuestion(quizElement){
+    quizElement.then(({question , options})=>{
+        let correctAnswerID = options.filter(opt=>opt.isCorrectAnswer)[0].id ; 
+        textQuestion.innerHTML= question.p ;  
+        imageQuestion.src= question.img; 
+        optionsParag.forEach((option  , index)=>{
+            option.innerHTML= options[index].countryInfo.name.common
+            option.dataset.id= options[index].id ; 
+            option.addEventListener('click' , ()=>{
+                displayResponse(correctAnswerID , option)
+                optionsParag.forEach(option=> removeOptionEventListeners(option)) 
+                lifesHandler(correctAnswerID , option) ;
+                lifesUIHandler()
+                removeOptionStyling(optionsParag)
+                setScore(correctAnswerID , option)
+                nextQuestion() ; 
+            },{once : true})
+        }) 
+    })
+}
+function removeOptionStyling(optionsElem){
+    optionsElem.forEach(option=>{
+        option.classList.remove("right-option")
+        option.classList.remove("wrong-option")
+    })
+}
+function nextQuestion(){
+    if(hearts > 0 ){
+
+        let question  = GenerateQuestion() ; 
+         displayQuestion(question) ; 
+    }else{
+        endQuiz()
+    }
+}
+function endQuiz(){
+    alert("endGame")
+}
+function setScore(correctAnswerID , option){
+    if(checkAnswer(correctAnswerID , option)){
+       score += 100 ; 
+    }
+    console.log(score);
+}
+function lifesHandler(correctAnswerID , option){
+    if(!checkAnswer(correctAnswerID , option)){
+        hearts -- ; 
+    }
+}
+function lifesUIHandler(){
+    console.log(hearts);
+    lifesElements.forEach((elem , index)=>{
+        if(index >= hearts){
+            elem.classList.add('gone')
+        }
+    })
+}
+function checkAnswer(correctAnswerID , option){
+    let anwserID = option.dataset.id ; 
+    return anwserID === correctAnswerID  ; 
+}
+function displayResponse(correctAnswerID , option){
+    if(checkAnswer(correctAnswerID , option)){
+        option.classList.add('right-option') 
+    }else{
+        option.classList.add('wrong-option') 
+        optionsParag.forEach(optionElement=>{
+            if(optionElement.dataset.id == correctAnswerID){
+                optionElement.classList.add('right-option') ; 
+            }
+        }); 
+
+    }
+}
+function  GenerateQuestion(){
     questionNbr++ ; 
-    levels.then(countries=>{
+  return  ( levels.then(countries=>{
                 if(questionNbr < 30){
                     countries = countries[0]
                 }else if(questionNbr < 60){
@@ -31,9 +112,25 @@ function  generateQuestion(){
                     countries = countries.flat()       
                 }
             let country = countries[random(0,countries.length)]
-            
-            })
+            let options = [
+                {id  : RandomId() , countryInfo  : getDiffCountry(countries,country) , isCorrectAnswer :  false},
+                {id  : RandomId() , countryInfo  : getDiffCountry(countries,country) , isCorrectAnswer :  false},
+                {id  : RandomId() , countryInfo  :  country  , isCorrectAnswer :  true},
+                {id  : RandomId() , countryInfo  : getDiffCountry(countries,country) , isCorrectAnswer :  false}
+                        ] ;
+            shuffle(options)                         
+            let quizQuestion = {
+                question : { p :"which country is this ?"  , img : country.flags.svg}  , 
+                options : options
+            }
+            // console.log(quizQuestion.options);
+            return quizQuestion
+            }) )
 }
+// function randomCountryAttribute(country){
+//     let hasAttribute = []
+    
+// } 
 function getDiffCountry(countries,country){
     let diffCountry  ; 
     do {
@@ -41,9 +138,30 @@ function getDiffCountry(countries,country){
     } while (diffCountry.cca3 == country.cca3);
     return diffCountry ; 
 }
+function removeOptionEventListeners(option) {
+    const newOption = option.cloneNode(true);
+    option.parentNode.replaceChild(newOption, option);
+     optionsParag = document.querySelectorAll('.option') ; 
+  }
+  
 function random(min =0, max) {
     return Math.floor( min + Math.random() * (max - min) );
+
+}
+function RandomId() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let randomId = '';
+  
+    for (let i = 0; i < 10; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      randomId += characters.charAt(randomIndex);
+    }
+  
+    return randomId;
   }
+function shuffle(array) {
+  array.sort(() => random(0,5)%2 == 0 ? -1 : 1 );
+}
 
 // function timer(duration){
 //     let circle = document.getElementById('cr'); 
